0205b50df32ca788f9d6fc908be9608dc858173c
Bernardo Resende (xWF) <deresendepinto@google.com>
2025-07-06 16:28:35 +0000

    Use the below as a base and implement changes in the current UI when app

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -3,7 +3,6 @@
 import { useToast } from "@/hooks/use-toast"
 import {
   Toast,
-  ToastClose,
   ToastDescription,
   ToastProvider,
   ToastTitle,
@@ -11,13 +10,13 @@ import {
 } from "@/components/ui/toast"
 
 export function Toaster() {
-  const { toasts } = useToast()
+  const { toasts, dismiss } = useToast()
 
   return (
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast key={id} {...props}>
+          <Toast key={id} {...props} onClick={() => dismiss(id)} className="cursor-pointer">
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
@@ -25,7 +24,6 @@ export function Toaster() {
               )}
             </div>
             {action}
-            <ToastClose />
           </Toast>
         )
       })}
